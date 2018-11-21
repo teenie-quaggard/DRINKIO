27704c69c3dff6b2b7936ee303498f4b55802be3
Whooolia <tpdms2779@gmail.com>
2018-11-21 17:19:45 +0000

    keypress works in dom.js

--- public/dom.js
+++ public/dom.js
@@ -1,3 +1,7 @@
-document.getElementById('beerInput').addEventListener('keypress', function(){
-    console.log('pressed');
+document.getElementById('beerInput').addEventListener('keypress', function(e){
+    if(e.keyCode == 13){
+        console.log('enter');
+    } else{
+        console.log(e.keyCode);
+    }
 })
